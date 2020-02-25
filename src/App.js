a2d344a31a8441fdf78ec2525a6ab69bb05ebccb
rbanh <rbanh@me.com>
2020-02-25 09:29:46 -0800

    layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from "react";
-import "./App.css";
 
 function App() {
   return (
     <div className='App'>
-      <ul>
-        <li>
-          <a href=''>Home</a>
-        </li>
-        <li>
-          <a href=''>Blog</a>
-        </li>
-        <li>
-          <a href=''>UX</a>
-        </li>
-        <li>
-          <a href=''>Code</a>
-        </li>
-        <li>
-          <a href=''>cRaFtHouse</a>
-        </li>
-      </ul>
+      <div className='sideMenu'>
+        <ul>
+          <li>
+            <a href=''>Home</a>
+          </li>
+          <li>
+            <a href=''>Blog</a>
+          </li>
+          <li>
+            <a href=''>UX</a>
+          </li>
+          <li>
+            <a href=''>Code</a>
+          </li>
+          <li>
+            <a href=''>cRaFtHouse</a>
+          </li>
+        </ul>
+      </div>
+      <div className='content'>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+      </div>
     </div>
   );
 }
