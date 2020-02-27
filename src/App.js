aa55c2714a8cffadba36ea8927926c9088b9ce8a
rbanh <rbanh@me.com>
2020-02-27 09:23:18 -0800

    need to figure out how to just display menu splash

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link} from "react-router-dom";
 import ContentBlog from "./Components/Blog";
 import Code from "./Components/Blog";
 import CraftHouse from "./Components/Crafthouse";
