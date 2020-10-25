6a55dbcf0627396a9e380d7ef44ae2e0458a4922
Richard Banh <rbanh@me.com>
2020-10-25 15:20:34 -0700

    layout of top splash

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -8,9 +8,22 @@ function Landing() {
   return (
     <div className='landing background_landing'>
       <div className='wrapper'>
-        <h1>Richard Banh</h1>
-        <img className="landing_profilepic" src={profilepic} alt=""/>
+        <div className='subtop'>
+          <h1>Richard Banh</h1>
+          <img className="landing_profilepic" src={profilepic} alt=""/>
+        </div>
+        <p className='text'>Looking to build cool stuff, <br/>for better or for worse; we learn something new and old ^..^</p>
+        <p className='font'>Mostly looking for a job but here are somethoughts here and there, maybe some illiterations from a faux literate. </p>
+        <div className='bottom-block'>
+          <div className='side'>
+            sfgasdfasdfas
+          </div>
+          <div className='side'>
+          dsfasdfasdfasdfadfs
+        </div>
+        </div>
       </div>
+      <div className='wrapper_bottom'></div>
     </div>
   )
 }
