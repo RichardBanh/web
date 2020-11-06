c1d1446b0b4f0f40b0ef353cff9b1d71aec7b4e4
Richard Banh <rbanh@me.com>
2020-11-06 14:34:27 -0800

    added blog page

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
-import Box from "./Blog/BoxBlog"
+import React, { Component } from "react";
+import Box from "./Blog/BoxBlog";
+import data from "../Data/Blog.json";
+import Box from "./Blog/BoxBlog";
 
 //place loop for data
 
 class ContentBlog extends Component {
-  state = {  }
-  render() { 
-    return ( <div>Blog</div> );
-  }
+	state = {};
+
+	render() {
+		const components = data.blog.map(({ date, text }) => (
+			<Box date={date} text={text} />
+		));
+		return <div className="specialB">{components}</div>;
+	}
 }
- 
+
 export default ContentBlog;
--- src/Components/Blog/BoxBlog.jsx
+++ src/Components/Blog/BoxBlog.jsx
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-function Box (props) {
-  return (
-    
-    <div className="EDIT_textblock">
-      <div className="date">{props.date}</div>
-      <div className="bottom_text_block">
-        <p className="text_bottom">{props.text}</p>
-      </div>
-      <div className="text_overlay"></div>
-    </div>
-    
-  )
+function Box(props) {
+	return (
+		<div className="EDIT_textblock">
+			<div className="date">{props.date}</div>
+			<div className="bottom_text_block">
+				<p className="text_bottom">{props.text}</p>
+			</div>
+			<div className="text_overlay"></div>
+		</div>
+	);
 }
- 
+
 export default Box;
